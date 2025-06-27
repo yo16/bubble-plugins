cab19fde03ff7a04107810b6591f063554016eb7
yo16 <xxyo1xx@gmail.com>
2025-06-27 11:18:45 +0900

    add GA-tag.

--- site-bubble-plugin/src/app/layout.tsx
+++ site-bubble-plugin/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import "@/app/globals.css";
 import theme from '@/lib/theme';
@@ -32,6 +33,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google tag (gtag.js) */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2TRHDP59NH"
+      ></Script>
+      <Script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2TRHDP59NH');
+          `,
+        }}
+      />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
